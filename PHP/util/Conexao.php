b1e530d1c62493ebfac05254d0df3543cbc26952
Ygor Gabriel Silva <silvaygor16@gmail.com>
2025-02-18 21:24:45 -0300

    Criar classe conexao

---
+++ PHP/util/Conexao.php
@@ -0,0 +1,28 @@
+<?php
+
+class Conexao{
+
+    private $localServer = "YGOR\SQLEXPRESS";
+    private $dataBase = "versami";
+    private $userServer = "sa";
+    private $serverPass = "Tc2088275";
+    private $connect;
+
+    public function connectDB(){
+
+        try{
+            $this->connect = new PDO("sqlsrv:server=$this->localServer; database=$this->dataBase",$this->userServer,$this->serverPass);
+
+        }catch(PDOException $e){
+
+            error_log("Erro de conexão " . $e.getMessage());
+
+        }
+
+        return $this->connect;
+    }
+
+    public function disconnectDB(){
+        $this->connect = null;
+    }
+}
