b64346264d94f29e288ba8c37e4b664ee08f84ba
Ygor Gabriel Silva <silvaygor16@gmail.com>
2025-02-20 12:52:13 -0300

    Criar classe postagem

---
+++ PHP/entities/Postagem.php
@@ -0,0 +1,17 @@
+<?php
+    class Postagem{
+
+        private title;
+        private content;
+        private like;
+        private postDate;
+        private reading;
+        private unread;
+        private read;
+
+        public function __construct(title, content){
+            
+        }
+
+        
+    }
