ac5c5638c16d511ae7aa105a91e17f0253b5b756
Ygor Gabriel Silva <silvaygor16@gmail.com>
2025-02-23 15:12:59 -0300

    Finalização dos modelos

---
+++ models/Admin.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'Perfil.php';
+class Admin extends Perfil{
+
+    private $admPermission;
+
+    public function __construct($name, $login, $email, $pass, $permission){
+        parent::__construct($name, $login, $email, $pass);
+        $this->setAdmPermission($permission);
+    }
+
+    public function getAdmPermission(){
+        return $this->admPermission;
+    }
+
+    public function setAdmPermission($permission){
+        $this->admPermission = $permission;
+    }
+}
